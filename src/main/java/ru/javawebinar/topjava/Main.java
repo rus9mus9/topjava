7faa79a23d25fcfc0ea8f2da8b6499c4c280c4fd
MURS <mussalimov46@gmail.com>
2017-07-06 19:26:21 +0500

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -7,8 +7,10 @@ package ru.javawebinar.topjava;
  * @link http://caloriesmng.herokuapp.com/
  * @link https://github.com/JavaOPs/topjava
  */
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         System.out.format("Hello Topjava Enterprise!");
     }
 }
