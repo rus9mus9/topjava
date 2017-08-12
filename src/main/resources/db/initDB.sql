deda99ccfd18c7116fa64645861fe55ec6cad355
MURS <mussalimov46@gmail.com>
2017-08-12 19:25:14 +0500

    Fixing tests, adding JDBC repo for the meals(Using Spring JDBC)

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,9 +1,9 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
-
 CREATE TABLE users
 (
   id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
@@ -21,5 +21,15 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  user_id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  calories INTEGER NOT NULL,
+  description VARCHAR NOT NULL,
+  dateTime TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,10 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (calories, description, dateTime, user_id) VALUES
+  (500, 'Завтрак', '2017-08-10 10:00:00', 100000),
+  (1000, 'Обед', '2017-08-10 14:00:00', 100000),
+  (250, 'Ужин', '2017-08-10 19:00:00', 100000),
+  (500, 'Завтрак', '2017-08-10 12:00:00', 100001),
+  (500, 'Обед', '2017-08-10 16:00:00', 100001);
